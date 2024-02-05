37aa121d21da2c22f3ef4409d6f2ebe44dd6f871
sjlvanq <correo@lode.uno>
2024-02-04 23:00:04 -0300

    Feat shortcode [catimg]. General minor changes and fixes

--- wp-category-image-plugin.php
+++ wp-category-image-plugin.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP Category Image plugin
 Description: Asocia una imagen a una categoría específica.
-Version: 0.1
+Version: dev-0.1
 Author: Silvano Emanuel Roqués
 Author URI: http://lode.uno/tejne
 */
@@ -10,13 +10,14 @@ Author URI: http://lode.uno/tejne
 function wpcip_category_image_field($taxonomies) {
 	// https://blog.wecodeyoursite.com/how-to-add-custom-field-to-wordpress-category/
          if (current_filter() == 'category_edit_form_fields') {  
-            $category_image = get_term_meta( $taxonomies->term_id, 'wpcip_image', true );  
+            $category_image = get_term_meta( $taxonomies->term_id, 'wpcip_image', true );
             ?>
 			<?php // https://themefoundation.com/wordpress-meta-boxes-guide/ //Aditional meta box input fields?>
 			<tr class="form-field">
 				<th valign="top" scope="row"><label for="wpcip_image" class="prfx-row-title"><?php _e( 'Category Image', 'wpcip' )?></label></th>
 				<td>
-				<input type="text" name="wpcip_image" id="wpcip_image" value="<?php if ( isset ( $category_image['wpcip-image'] ) ) echo $category_image['wpcip-image'][0]; ?>" />
+				<img src="<?php echo $category_image; ?>" />
+				<input type="text" name="wpcip_image" id="wpcip_image" value="<?php if ( isset ( $category_image) ) echo $category_image; ?>" />
 				<input type="button" id="wpcip-image-button" class="button" value="<?php _e( 'Choose or Upload an Image', 'wpcip' )?>" />
 				</td>
 			</tr>
@@ -47,7 +48,7 @@ function wpcip_save_category_image_field($term_id) {
 add_action('edited_category', 'wpcip_save_category_image_field', 10, 2);
 add_action('create_category', 'wpcip_save_category_image_field', 10, 2); 
 
-function wpcip_image_enqueue() {
+function wpcip_enqueue_script() {
     global $typenow;
     if( $typenow == 'post' ) {
         wp_enqueue_media();
@@ -63,13 +64,20 @@ function wpcip_image_enqueue() {
         wp_enqueue_script( 'meta-box-image' );
     }
 }
-add_action( 'admin_enqueue_scripts', 'wpcip_image_enqueue' );
+add_action( 'admin_enqueue_scripts', 'wpcip_enqueue_script' );
 
-function display_category_image(){	
+function wpcip_shortcode( $atts = [], $content = null) {
     if(is_category()){
-        $termid = get_queried_object()->term_id;			
-        $category_image =get_term_meta( $termid, 'wpcip_image', true );			
-    }
-    echo '<!-- La imagen de la categoría es '. $category_image.' -->';
+		$termid = get_queried_object()->term_id;
+		$src = get_term_meta( $termid, 'wpcip_image', true );
+		if(!empty($src)){
+			$alt = $atts["alt"];
+			$width = $atts["width"];
+			$height = $atts["height"];
+			$content = '<img src="'.$src.'" alt="'.$alt.'" width="'.$width.'" height="'.$height.'" />';
+			return $content;
+		}
+	}
 }
-//add_action('........','.....................',1);
+
+add_shortcode('catimg', 'wpcip_shortcode');
